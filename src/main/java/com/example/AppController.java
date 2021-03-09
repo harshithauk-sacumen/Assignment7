0c7c9caf17bf754792332544716e7dae01f463f2
Harshitha <harshitha.uk@sacumen.com>
2021-03-09 16:23:58 +0530

    My Third Commit

---
+++ src/main/java/com/example/AppController.java
@@ -0,0 +1,35 @@
+package com.example.springbootfirstapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "register_success";
+    }
+}
---
+++ src/test/java/com/example/springbootfirstapp/SpringbootFirstAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbootfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringbootFirstAppApplicationTests {
+	public static void main(String[] args) {
+		/*@Test
+		void contextLoads () {
+		}*/
+		SpringApplication.run(SpringbootFirstAppApplicationTests.class, args);
+	}
+}
